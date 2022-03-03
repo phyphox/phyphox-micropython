54b506d60947a5c2921b9761e3a742b23f938cac
edwardleier <74976556+edwardleier@users.noreply.github.com>
2022-03-03 15:46:19 +0100

    Create randomNumbers.py

---
+++ randomNumbers.py
@@ -0,0 +1,16 @@
+import phyphoxBLE
+import time
+import random
+
+def main():
+    p = phyphoxBLE.PhyphoxBLE()
+    p.start()                                #Start the BLE server
+        
+    while True:
+        randomNumber = random.randint(0,100) #Generate random number in the range 0 to 100
+        
+        p.write(randomNumber)                #Send value to phyphox
+        time.sleep_ms(500)                   #Shortly pause before repeating
+            
+if __name__ == "__main__":
+    main()
